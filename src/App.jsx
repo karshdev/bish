bb1e0880e0baf227b49893abb124d7ae0fc0d270
karshdev <sharmaaakarsh332@gmail.com>
2025-01-07 22:08:48 +0530

    fixed overflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Homepage from './pages/homepage/Hompage'
 import Card from './pages/homepage/Cards/Card'
